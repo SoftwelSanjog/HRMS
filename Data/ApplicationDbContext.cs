4fec666c9de629a5a02e2901ef96e7cc2b65c45f
Sanjog Shakya <softwel.sanjog@gmail.com>
2024-05-02 12:57:23 +0545

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HRMS.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace HRMS.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        public string EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {MiddleName}  {LastName}";
+        public int PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; } 
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Cluster {  get; set; }
+        public string Designation { get; set; }
+
+    }
+}
